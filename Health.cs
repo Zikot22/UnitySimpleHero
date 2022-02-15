0bb6691efe2428d6f697e50d20c3c5560cc1d3db
Zikot22 <73202799+Zikot22@users.noreply.github.com>
2022-02-15 23:13:20 +0300

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    [Header ("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numbersOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                dead = true;
+                if (gameObject.tag == "Player")
+                {
+                    GetComponent<HeroMovement>().enabled = false;
+                }
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        for (int i = 0; i < numbersOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration / (numbersOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numbersOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+    }
+}
